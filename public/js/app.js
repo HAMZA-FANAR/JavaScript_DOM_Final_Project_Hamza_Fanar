3b81108711cd00daee3800d15142cac9c58ee621
HAMZA-FANAR <hamzapablo2000@gmail.com>
2024-08-15 15:11:48 +0100

    add modal

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
+// button video
 let playButton = document.querySelector(".btn-video");
 let video = document.querySelector("video");
 playButton.addEventListener("click", ()=> {
     video.play();
     playButton.style.display = "none";
-    
-    
 });
+// end button video
+
 
 
 
@@ -23,3 +24,33 @@ document.querySelector(".btn3-menu").addEventListener("click", ()=> {
 document.querySelector(".btn4-menu").addEventListener("click", ()=> {
     document.querySelector(".chec-menu").innerHTML = "Dinner";
 });
+// end menu
+
+
+
+// modal
+var modal = document.querySelector('#simpleModal');
+
+var open = document.querySelector('#openModalBtn');
+
+var close = document.querySelector('.closeBtn');
+
+open.addEventListener('click', function openModal() {
+    modal.style.display = 'block';
+});
+
+close.addEventListener('click', function closeModal() {
+    modal.style.display = 'none';
+
+});
+
+window.addEventListener('click', outsideClick);
+
+
+
+function outsideClick(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none';
+    }
+}
+// end modal
